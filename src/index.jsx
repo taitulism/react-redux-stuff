448e5eb5a5a4d78dd34c79e18cea5f8479e6ae09
Itai Tenenbaum <taitulism@gmail.com>
2017-12-08 05:27:03 +0200

    fix tap error

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
+import injectTapEventPlugin from "react-tap-event-plugin";
 
 import App from '../containers/App';
 import configureStore from '../store/configureStore';
 
 //Needed for React Developer Tools
 window.React = React;
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 const store = configureStore();
 
